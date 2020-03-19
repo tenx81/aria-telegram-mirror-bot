4f0f25a5df661df9a9ae120d455e883ae4ca0a00
Parveen Bhadoo <bhadoo@pm.me>
2020-03-19 14:09:50 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const bot = new TelegramBot(constants.TOKEN, { polling: true });
 var websocketOpened = false;
 var statusInterval: NodeJS.Timeout;
 var dlManager = dlm.DlManager.getInstance();
-
+var hosts = ['https://api.telegram.org'];
 initAria2();
 
 bot.on("polling_error", msg => console.error(msg.message));
@@ -34,9 +34,10 @@ function setEventCallback(regexp: RegExp, regexpNoName: RegExp,
 
 setEventCallback(eventRegex.commandsRegex.start, eventRegex.commandsRegexNoName.start, (msg) => {
   if (msgTools.isAuthorized(msg) < 0) {
-    msgTools.sendUnauthorizedMessage(bot, msg);
+    // msgTools.sendUnauthorizedMessage(bot, msg);
+    msgTools.sendMessage(bot, msg, 'Works only inside @BhadooCloud (Bhadoo Cyber Systems, Model 101)', -1);
   } else {
-    msgTools.sendMessage(bot, msg, 'You should know the commands already. Happy mirroring.', -1);
+    msgTools.sendMessage(bot, msg, 'All Systems Up and Running', -1);
   }
 });
 
@@ -92,7 +93,7 @@ setEventCallback(eventRegex.commandsRegex.mirrorStatus, eventRegex.commandsRegex
   if (msgTools.isAuthorized(msg) < 0) {
     msgTools.sendUnauthorizedMessage(bot, msg);
   } else {
-    sendStatusMessage(msg);
+    sendStatusMessage(msg , true);
   }
 });
 
@@ -283,11 +284,22 @@ function prepDownload(msg: TelegramBot.Message, match: string, isTar: boolean): 
       console.log(`gid: ${gid} download:${match}`);
       // Wait a second to give aria2 enough time to queue the download
       setTimeout(() => {
-        dlManager.setStatusLock(msg, sendStatusMessage);
+        // dlManager.setStatusLock(msg, sendStatusMessage);
+        dlManager.setStatusLock(msg, uriAdded);
       }, 1000);
     }
   });
 
+}
+
+/**
+ * 
+ * Added mirror function
+ * send a added mirror msg --- added by @aryanvikash
+ */
+
+function uriAdded(msg: TelegramBot.Message): any{
+  msgTools.sendMessage(bot, msg, 'URI Added 😊,\nClick /mirrorstatus to get Status.', -1);
 }
 
 /**
@@ -429,7 +441,7 @@ function ariaOnDownloadStart(gid: string, retry: number): void {
 
     if (!statusInterval) {
       statusInterval = setInterval(updateAllStatus,
-        constants.STATUS_UPDATE_INTERVAL_MS ? constants.STATUS_UPDATE_INTERVAL_MS : 12000);
+        constants.STATUS_UPDATE_INTERVAL_MS ? constants.STATUS_UPDATE_INTERVAL_MS :12000);
     }
   } else if (retry <= 8) {
     // OnDownloadStart probably got called before prepDownload's startDownload callback. Fairly common. Retry.
@@ -554,7 +566,6 @@ function initAria2(): void {
   ariaTools.setOnDownloadError(ariaOnDownloadError);
 }
 
-
 function driveUploadCompleteCallback(err: string, gid: string, url: string, filePath: string,
   fileName: string, fileSize: number, isFolder: boolean): void {
 
@@ -568,13 +579,16 @@ function driveUploadCompleteCallback(err: string, gid: string, url: string, file
     console.log(`${gid}: Uploaded `);
     if (fileSize) {
       var fileSizeStr = downloadUtils.formatSize(fileSize);
-      finalMessage = `<a href='${url}'>${fileName}</a> (${fileSizeStr})`;
+      if(url.indexOf("/folders/") > -1){
+        var rawurl = constants.INDEX_DOMAIN + fileName + "/";
+      }else{
+        var rawurl = constants.INDEX_DOMAIN + fileName ;
+      }
+      var indexurl = encodeURI(rawurl) ;
+      finalMessage = `Drive Link: \n<a href='${url}'>${fileName}</a> (${fileSizeStr}) \n \nShare Link: \n<a href='${indexurl}'>${fileName}</a>`;
     } else {
-      finalMessage = `<a href='${url}'>${fileName}</a>`;
-    }
-    if (constants.IS_TEAM_DRIVE && isFolder) {
-      finalMessage += '\n\n<i>Folders in Shared Drives can only be shared with members of the drive. Mirror as an archive if you need public links.</i>';
+      finalMessage = `Drive Link: <a href='${url}'>${fileName}</a> \n \nShare Link: \n<a href='${indexurl}'>${fileName}</a>`;
     }
     cleanupDownload(gid, finalMessage, url);
+    }
   }
-}
